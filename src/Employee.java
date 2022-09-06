378c7c6c5ded24495720a2ccad41b94c5820b1b6
darwhais <darwhais@kkg-do.eu>
2022-09-06 11:25:05 +0200

    kjik

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,13 @@
 public class Employee {
+
+    private String name;
+    private String vorname;
+    private String geburtsdatum;
+    private String geschlecht;
+    private int alter;
+    private double gewicht;
+    private String abteilung;
+    private double gehalt;
+    private int dienstjahre;
+    private String besonderheiten;
 }
